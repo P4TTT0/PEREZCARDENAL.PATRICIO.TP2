de4987018d80b6636e6ef2e90fe728b7895c50b5
Patricio Perez Cardenal <patricio.perezcardenal@gmail.com>
2022-10-27 22:01:39 -0300

    V-0.01 [INICIO DE PARCIAL_02 (JUEGO: TRUCO)]

---
+++ PARCIAL_02[PEREZCARDENAL.PATRICIO]/PARCIAL_02[PEREZCARDENAL.PATRICIO]/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PARCIAL_02_PEREZCARDENAL.PATRICIO_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
